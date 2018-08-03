a22212abaa9c2e05b4cafe43d0c523d3246c6564
Spencer Charest <SpencerC77@gmail.com>
2018-08-02 19:54:03 -0700

    add cheerio and add function to get page content

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import Promise from 'bluebird';
 import TelegramBot from 'node-telegram-bot-api';
+import axios from 'axios';
+import cheerio from 'cheerio';
 import dotenv from 'dotenv';
 import path from 'path';
 
@@ -14,11 +17,28 @@ function getLinkURLs(text) {
   return text.match(/\bhttps?:\/\/\S+/gi);
 }
 
+async function getPageContent(url) {
+  // get html
+  const { data } = await axios.get(url);
+  // load into cheerio
+  const $ = cheerio.load(data);
+  // get image source and post title
+  const image = $('img').attr('src');
+  const title = $('h2').text();
+
+  return {
+    title,
+    image,
+  };
+}
+
 bot.on('message', async (msg) => {
   const { text } = msg;
   // only run for reddit posts
   if (text.includes('reddit.com')) {
     // get link urls from text
     const linkURLs = getLinkURLs(text);
+    // get page content
+    const content = await Promise.map(linkURLs, url => getPageContent(url));
   }
 });
